b3897fe12a74495f0f093c48e32c6f5f83d10620
Vajda Örs <ors0092@gmail.com>
2024-07-23 19:20:43 +0200

    Update init.sh
    add version check for neofetch (pre-trixie) and fastfetch (trixie and greater)
    add additional packages
    remove duploicate nala in pkgnames as it was already installed

--- init.sh
+++ init.sh
@@ -4,6 +4,16 @@ echo -e "\n\nFIGYELEM-FIGYELEM-FIGYELEM!\nNe felejtsd el manuálisan kezelni:\n*
 
 sudo apt update && sudo apt -y install nala # Frissíti a csomaglistát és telepíti a nala csomagkezelőt
 
+# Ellenőrizzük a Debian verziót
+debian_version=$(lsb_release -cs)
+
+# Ha a verzió Trixie vagy újabb, akkor fastfetch, egyébként neofetch
+if [[ "$debian_version" == "trixie" ]]; then
+    sysinfo_tool="fastfetch"
+else
+    sysinfo_tool="neofetch"
+fi
+
 pkgnames=(
     vlc                 # VLC médialejátszó, különféle formátumokat támogat
     powerdevil          # KDE energia kezelő, segít az akkumulátor és energia beállítások kezelésében
@@ -11,11 +21,20 @@ pkgnames=(
     timeshift           # Rendszer visszaállító eszköz, hasznos rendszermentések készítéséhez
     rdfind              # Duplikált fájlok keresése és eltávolítása a rendszeren
     zsh                 # Z shell, egy erőteljes parancssori héj
-    fastfetch           # Rendszerinformációk gyors megjelenítése a terminálban
-    nala                # Felhasználóbarát APT frontend, jobb csomagkezelés érdekében
+    "$sysinfo_tool"     # Rendszerinformációk megjelenítése (fastfetch vagy neofetch a Debian verziótól függően)
+    krename             # Tömeges fájlátnevező eszköz
     partitionmanager    # KDE partíciókezelő, segít a lemezpartíciók kezelésében
     filelight           # Lemezhasználat elemző, vizuálisan megjeleníti a fájlok és könyvtárak méretét
     synaptic            # Grafikus csomagkezelő, egyszerűbbé teszi a csomagok telepítését és eltávolítását
+    inxi                # Rendszerinformációs eszköz
+    kodi                # Médialejátszó és szórakoztató központ
+    kwin-addons         # KDE ablakkezelő kiegészítések
+    gamemode            # Rendszer teljesítményének optimalizálása játék közben
+    plasma-gamemode     # Integráció a GameMode-dal KDE Plasma alatt
+    lutris              # Nyílt forráskódú játékkezelő
+    qbittorrent         # Bittorrent kliens
+    neochat             # KDE Matrix kliens
+    thunderbird         # E-mail kliens
 )
 
 sudo nala install -y "${pkgnames[@]}"
